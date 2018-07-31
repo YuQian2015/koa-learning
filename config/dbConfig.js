68d4bf8238ade869e24f99128ac73b0054ea2fc1
yuqian <757275030@qq.com>
2018-07-31 21:09:14 +0800

    acl

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,27 +2,14 @@ let mongoose = require('mongoose');
 const config = require('config');
 const dbConfig = config.get('Database');
 let acl = require('acl');
-var mongodb = require('mongodb');
+
+// 如果使用 mongodb
+// var mongodb = require('mongodb');
 // mongodb.connect(`mongodb://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}?authSource=${dbConfig.dbName}`, function(error, db) {
-//   console.log(error);
-//   var mongoBackend = new acl.mongodbBackend(db, 'acl_');
-//
+//   console.log(db.db(`${dbConfig.dbName}`));
+//   var mongoBackend = new acl.mongodbBackend(db.db(`${dbConfig.dbName}`), 'acl_');
 //   acl = new acl(mongoBackend);
 //   acl.allow('guest', 'blogs', 'view')
-//
-//   // allow function accepts arrays as any parameter
-//   acl.allow('member', 'blogs', ['edit', 'view', 'delete'])
-//
-//   acl.addUserRoles('joed', 'guest')
-//
-//   acl.addRoleParents('baz', ['foo', 'bar'])
-//
-//   // acl.isAllowed('joed', 'blogs', 'view', function(err, res){
-//   //   console.log(123132131);
-//   //     if(res){
-//   //         console.log("User joed is allowed to view blogs")
-//   //     }
-//   // })
 // });
 
 exports.connect = (request, response) => {
@@ -34,32 +21,27 @@ exports.connect = (request, response) => {
   db.once('open', (callback) => {
     console.log(`Mongoose连接到${dbConfig.dbName}`);
 
+    console.log(db.db);
+    const mongoBackend = new acl.mongodbBackend(db.db, 'acl_', {
+      useSingle: true
+    });
+    acl = new acl(mongoBackend);
 
-    // const mongoBackend = new acl.mongodbBackend(db, 'acl_', {
-    //   useSingle: true
-    // });
-    // console.log("___________________________");
-    // acl = new acl(mongoBackend);
-
-
-    // acl = new acl(new acl.memoryBackend());
     // guest is allowed to view blogs
+    acl.allow('guest', 'blogs', 'view')
 
+    // allow function accepts arrays as any parameter
+    acl.allow('member', 'blogs', ['edit', 'view', 'delete'])
 
-    // acl.allow('guest', 'blogs', 'view')
-    //
-    // // allow function accepts arrays as any parameter
-    // acl.allow('member', 'blogs', ['edit', 'view', 'delete'])
-    //
-    // acl.addUserRoles('joed', 'guest')
-    //
-    // acl.addRoleParents('baz', ['foo', 'bar'])
-    //
-    // acl.isAllowed('joed', 'blogs', 'view', function(err, res){
-    //   console.log(123132131);
-    //     if(res){
-    //         console.log("User joed is allowed to view blogs")
-    //     }
-    // })
+    acl.addUserRoles('joed', 'guest')
+
+    acl.addRoleParents('baz', ['foo', 'bar'])
+
+    acl.isAllowed('joed', 'blogs', 'view', function(err, res){
+      console.log(123132131);
+        if(res){
+            console.log("User joed is allowed to view blogs")
+        }
+    })
   });
 }
